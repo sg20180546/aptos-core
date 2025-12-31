4157c47fbed9987b040405a0bd1eaf2b09b0051c
sj <sg20180546@gmail.com>
2026-01-01 02:45:30 +0900

    println

--- execution/executor-benchmark/src/measurements.rs
+++ execution/executor-benchmark/src/measurements.rs
@@ -523,9 +523,9 @@ impl OverallMeasurement {
         let mut all_latencies = get_all_latencies();
         if !all_latencies.is_empty() {
             let percentiles = calculate_percentiles_us(&mut all_latencies);
-            println("{} === Transaction Tail Latencies (microseconds) ===\n", self.prefix);
+            println!("{} === Transaction Tail Latencies (microseconds) ===\n", self.prefix);
             for (label, value_us) in percentiles.iter() {
-                println("{} {}: {} us\n", self.prefix, label, value_us);
+                println!("{} {}: {} us\n", self.prefix, label, value_us);
             }
         }
     }
